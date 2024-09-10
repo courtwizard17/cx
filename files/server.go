3f004fb97f7564899221f1ffe706ba6d49d44f44
cw <courtwizard17@pm.me>
2024-09-10 15:38:30 +0300

    error handling to file server

--- files/server.go
+++ files/server.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	http.Handle("/docs/", http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs"))))
+	http.HandleFunc("/", handleRequest)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[1:]
+
+	if _, err := os.Stat(filepath.Join("docs", path)); os.IsNotExist(err) {
+		// File doesn't exist, return custom message
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "This file doesn't exist.")
+		return
+	}
+
+	http.ServeFile(w, r, filepath.Join("docs", path))
 }
