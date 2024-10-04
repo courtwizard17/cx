954690595247bf0b37c3f9229833e17f1d2ebdb6
cw <courtwizard17@pm.me>
2024-10-04 13:01:57 +0300

    server final

--- osrs/server.go
+++ osrs/server.go
@@ -4,14 +4,25 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", serveOSRS)
-	http.Handle("/osrs/", http.StripPrefix("/osrs/", http.FileServer(http.Dir("/app/"))))
-
+	http.HandleFunc("/", handleRequest)
 	log.Println("Server starting on :8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.URL.Path, "/osrs") {
+		if r.URL.Path != "/osrs" {
+			http.Redirect(w, r, "/osrs", http.StatusMovedPermanently)
+			return
+		}
+		serveOSRS(w, r)
+	} else {
+		http.NotFound(w, r)
+	}
 }
 
 func serveOSRS(w http.ResponseWriter, r *http.Request) {
@@ -24,4 +35,8 @@ func serveOSRS(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving file: %s", htmlPath)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	http.ServeFile(w, r, htmlPath)
+}
+
+func init() {
+	http.Handle("/osrs/", http.StripPrefix("/osrs/", http.FileServer(http.Dir("/app/"))))
 }
